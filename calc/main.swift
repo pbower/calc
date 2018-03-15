2d4eb4c9e7d35f23efcbd1c4604ed49908b281df
Peter Bower <doctor_bower@hotmail.com>
2018-03-15 21:03:32 +1100

    CheckPt 1
    Check point 1:
    Added calculate Function
    Added Switch Statement for calculation

--- calc/main.swift
+++ calc/main.swift
@@ -7,8 +7,37 @@
 //
 
 import Foundation
-
 var args = ProcessInfo.processInfo.arguments
 args.removeFirst() // remove the name of the program
 
 print(Int(args[0])!)
+
+/*
+ class InputProcessing {
+    func processInput() {
+    }
+}
+ */
+
+
+func calculate(_ firstNum: Int,_ secondNum: Int,_ modifier: Character) -> Int? {
+    var result: Int
+    switch modifier {
+    case "+":
+        result = firstNum + secondNum
+    case "-":
+        result = firstNum - secondNum
+    case "x":
+        result = firstNum * secondNum
+    case "/":
+        result = firstNum / secondNum
+    case "%":
+        result = firstNum % secondNum
+    default:
+        return nil
+    }
+    return result
+}
+
+let myCalc = calculate(36, 24, "x")
+print(myCalc ?? "Error")
